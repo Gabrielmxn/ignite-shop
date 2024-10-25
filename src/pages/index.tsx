d8e9ab8665e5d26cd86556d38c48b62f9a5d38bb
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2024-10-25 14:41:19 -0300

    :sparkles: feat: Estrutura visual - Estrutura da Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,29 @@
-import { styled } from "@/styles";
+import { HomeContainer, Product } from "@/styles/pages/home";
+import Image from "next/image";
 
-const Button = styled('button', {
-  backgroundColor: "$rocketseat",
-  borderRadius: 4,
-  padding: '4px 8px',
-  border: 0,
-  color: "$green500",
-
-  span: {
-    fontWeight: 'bold'
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  }
-})
+import camiseta1 from '@/assets/shirt1.png'
+import camiseta2 from '@/assets/shirt2.png'
+import camiseta3 from '@/assets/shirt3.png'
 
 export default function Home() {
   return (
-    <Button>
-      Enviar
-      <span>Teste</span>
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 80,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 }
