9bd0bd5695096922c1cbf843e619091d730e4f5a
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2024-10-29 22:18:02 -0300

    :sparkles: feat: Produto & Checkout - Estrutura da página de produto[

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,8 +1,24 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product"
+import Image from "next/image"
 import { useRouter } from "next/router"
 
 export default function Product() {
   const router = useRouter()
   return (
-    <h1>{router.query.id}</h1>
+    <ProductContainer>
+      <ImageContainer>
+        {/* <Image /> */}
+
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus dolor deleniti voluptate distinctio dolore tempore, sit quaerat expedita minima ab possimus repellendus illum omnis fuga! Blanditiis tempora possimus omnis enim.</p>
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
